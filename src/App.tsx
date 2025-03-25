7ce19369a9b7612bf6b554dc1a71c3bd4918c87a
avalenzo1 <anthonyvalenzo4@gmail.com>
2025-03-25 12:24:42 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue">HELLO WORLD!!</h1>
+
+      <button className="btn">CLICK ME!!!</button>
+    </>
+  )
+}
+
+export default App
